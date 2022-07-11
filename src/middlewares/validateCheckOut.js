182ccbdad02eaff16a3e4ef6766157437fe92f37
laracamarino <laracamarino@gmail.com>
2022-07-11 14:57:14 -0300

    third attempt to validateCheckOut

--- src/middlewares/validateCheckOut.js
+++ src/middlewares/validateCheckOut.js
@@ -1,20 +1,18 @@
 import joi from "joi";
 
 export default async function validateCheckOut(req, res, next) {
-    const {purchaseDetails} = req.body;
+    const { purchaseDetails } = req.body;
     const nameRegex = /^[a-zA-Z]{2,25}$/;
     const cardNumberRegex = /^[0-9]{16}$/;
     const cvcRegex = /^[0-9]{3}$/;
 
-    const purchaseSchema = joi.object.keys({
-        purchaseDetails: {
-            firstName: joi.string().pattern(nameRegex).required(),
-            lastName: joi.string().pattern(nameRegex).required(),
-            email: joi.string().email().required(),
-            cardNumber: joi.string().pattern(cardNumberRegex).required(),
-            expirationDate: joi.string().required(),
-            cvc: joi.string().pattern(cvcRegex).required()
-        }
+    const purchaseSchema = joi.object({
+        firstName: joi.string().pattern(nameRegex).required(),
+        lastName: joi.string().pattern(nameRegex).required(),
+        email: joi.string().email().required(),
+        cardNumber: joi.string().pattern(cardNumberRegex).required(),
+        expirationDate: joi.string().required(),
+        cvc: joi.string().pattern(cvcRegex).required()
     });
     const validation = purchaseSchema.validate(purchaseDetails, { abortEarly: false });
     if (validation.error) {
