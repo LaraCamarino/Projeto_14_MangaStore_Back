8f4a9aeae6bdec01fddd9b9498e08558544dd585
laracamarino <laracamarino@gmail.com>
2022-07-11 04:56:22 -0300

    fixing errors

--- src/routes/checkoutRoutes.js
+++ src/routes/checkoutRoutes.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import { checkOut } from "../controllers/checkOutController.js";
 import validateToken from "../middlewares/validateToken.js";
-import validateCheckOut from "../middlewares/validateCheckOut.js";
 
 const router = Router();
 
-router.post("/purchase", validateToken, validateCheckOut, checkOut);
+router.post("/purchase", validateToken, checkOut);
 
 export default router;
