6a5d4e2a625cae6108d770efb91742756f478ebf
laracamarino <laracamarino@gmail.com>
2022-07-11 14:41:32 -0300

    second attempt to validateCheckOut

--- src/middlewares/validateCheckOut.js
+++ src/middlewares/validateCheckOut.js
@@ -3,17 +3,17 @@ import joi from "joi";
 export default async function validateCheckOut(req, res, next) {
     const {purchaseDetails} = req.body;
     const nameRegex = /^[a-zA-Z]{2,25}$/;
-    const cardNumberRegex = /^.{16}$/;
-    const cvcRegex = /^.{3}$/;
+    const cardNumberRegex = /^[0-9]{16}$/;
+    const cvcRegex = /^[0-9]{3}$/;
 
     const purchaseSchema = joi.object.keys({
         purchaseDetails: {
             firstName: joi.string().pattern(nameRegex).required(),
             lastName: joi.string().pattern(nameRegex).required(),
             email: joi.string().email().required(),
-            cardNumber: joi.number().pattern(cardNumberRegex).required(),
+            cardNumber: joi.string().pattern(cardNumberRegex).required(),
             expirationDate: joi.string().required(),
-            cvc: joi.number().pattern(cvcRegex).required()
+            cvc: joi.string().pattern(cvcRegex).required()
         }
     });
     const validation = purchaseSchema.validate(purchaseDetails, { abortEarly: false });
